f8ec3110b88802f42b0d76dfadf24682bd2ee2ea
Maria Abramovich <57722806+Dafery@users.noreply.github.com>
2023-03-30 00:23:22 +0300

    Delete src directory

--- src/js/const.js
+++
@@ -1,9 +0,0 @@
-import Bowman from './characters/Bowman';
-import Daemon from './characters/Daemon';
-import Magician from './characters/Magician';
-import Swordsman from './characters/Swordsman';
-import Undead from './characters/Undead';
-import Vampire from './characters/Vampire';
-
-export const charactersUser = [Bowman, Swordsman, Magician];
-export const charactersAI = [Vampire, Undead, Daemon];
